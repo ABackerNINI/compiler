2c215e919de68a6ab0dab8bf4dd5fc60aff59f12
ABacker <1105908317@qq.com>
2017-04-06 20:35:17 +0800

    add keywords

---
+++ Common/Keywords.h
@@ -0,0 +1,97 @@
+//
+// Created by nini on 4/6/17.
+//
+// Keywords defined in ISO:14882
+
+#ifndef COMPILER_KEYWORDS_H
+#define COMPILER_KEYWORDS_H
+
+//2.12 Keywords:Table 4 - Keywords
+#define KW_ALIGNAS  1
+#define KW_ALIGNOF  2
+#define KW_ASM      3
+#define KW_AUTO     4
+#define KW_BOOL 5
+#define KW_BREAK 6
+#define KW_CASE 7
+#define KW_CATCH 8
+#define KW_CHAR 9
+#define KW_CHAR16_T 10
+#define KW_CHAR32_T 11
+#define KW_CLASS 12
+#define KW_CONST 13
+#define KW_CONSTEXPR 14
+#define KW_CONST_CAST 15
+#define KW_CONTINUE 16
+#define KW_DECLTYPE 17
+#define KW_DEFAULT 18
+#define KW_DELETE 19
+#define KW_DO 20
+#define KW_DOUBLE 21
+#define KW_DYNAMIC_CAST 22
+#define KW_ELSE 23
+#define KW_ENUM 24
+#define KW_EXPLICIT 25
+#define KW_EXPORT 26
+#define KW_EXTERN 27
+#define KW_FALSE 28
+#define KW_FLOAT 29
+#define KW_FOR 30
+#define KW_FRIEND 31
+#define KW_GOTO 32
+#define KW_IF 33
+#define KW_INLINE 34
+#define KW_INT 35
+#define KW_LONG 36
+#define KW_MUTABLE 37
+#define KW_NAMESPACE 38
+#define KW_NEW 39
+#define KW_NOEXCEPT 40
+#define KW_NULLPTR 41
+#define KW_OPERATOR 42
+#define KW_PRIVATE 43
+#define KW_PROTECTED 44
+#define KW_PUBLIC 45
+#define KW_REGISTER 46
+#define KW_REINTERPRET_CAST 47
+#define KW_RETURN 48
+#define KW_SHORT 49
+#define KW_SIGNED 50
+#define KW_SIZEOF 51
+#define KW_STATIC 52
+#define KW_STATIC_ASSERT 53
+#define KW_STATIC_CAST 54
+#define KW_STRUCT 55
+#define KW_SWITCH 56
+#define KW_TEMPLATE 57
+#define KW_THIS 58
+#define KW_THREAD_LOCAL 59
+#define KW_THROW 60
+#define KW_TRUE 61
+#define KW_TRY 62
+#define KW_TYPEDEF 63
+#define KW_TYPEID 64
+#define KW_TYPENAME 65
+#define KW_UNION 66
+#define KW_UNSIGNED 67
+#define KW_USING 68
+#define KW_VIRTUAL 69
+#define KW_VOID 70
+#define KW_VOLATILE 71
+#define KW_WCHAR_T 72
+#define KW_WHILE 73
+
+//2.12 Keywords:Table 5 - Alternative representations
+#define KW_AND 74
+#define KW_AND_EQ 75
+#define KW_BITAND 76
+#define KW_BITOR 77
+#define KW_COMPL 78
+#define KW_NOT 79
+#define KW_NOT_EQ 80
+#define KW_OR 81
+#define KW_OR_EQ 82
+#define KW_XOR 83
+#define KW_XOR_EQ 84
+
+#endif //COMPILER_KEYWORDS_H
