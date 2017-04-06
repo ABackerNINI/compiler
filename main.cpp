28e9dc3d4da10259dce51a2c990dfe5ff82ab7d8
ABacker <1105908317@qq.com>
2017-04-06 20:35:31 +0800

    save

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 #include "Preprocessor/Preprocessor.h"
 
 int main() {
-    compiler::preprocessor::Preprocess("/home/abacker/ClionProjects/Compiler/Utility/String.h");
+
+    //compiler::preprocessor::Preprocess("/home/abacker/ClionProjects/Compiler/Utility/String.h");
 
     return 0;
 }
